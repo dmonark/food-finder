f72d6aeec42ea2a91badda1e7ad219171a89f5b4
MonarkND <monarkdedakiya@gmail.com>
2018-07-20 23:08:18 +0530

    New UI added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from "react";
-import Footer from "./component/footer";
-import NavBar from "./component/navBar";
 import Foodie from "./component/foodie";
 
 class App extends React.Component {
--- src/component/foodie.js
+++ src/component/foodie.js
@@ -4,12 +4,6 @@ import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
-import Badge from "@material-ui/core/Badge";
-
-var zomato = require('zomato');
-var client = zomato.createClient({
-    userKey: '99602fd2da0b1d1a976d947037a4bcd6', 
-});
 
 class Foodie extends React.Component {
     
@@ -20,23 +14,47 @@ class Foodie extends React.Component {
             resData: [],
             whichRes: null
         };
+
+        this.dataFetcher = this.dataFetcher.bind(this);
     }
 
-    componentDidMount(){
-        var resData = []
-        client.getGeocode({
-            lat:"13.0827",
-            lon:"80.2707",
-        }, function(err, result){
-            if(!err){
-                resData = JSON.parse(result).nearby_restaurants;         
+    dataFetcher(count){
+        var resData = this.state.resData;
+        
+        fetch("https://developers.zomato.com/api/v2.1/search?entity_id=7&entity_type=city&start="+count, {
+            headers: {
+                "Content-Type": "application/json; charset=utf-8",
+                "user-key": "99602fd2da0b1d1a976d947037a4bcd6"
+            },
+        })
+        .then(res => res.json())
+        .then(
+            (result) => {
+                var resDataRaw = result.restaurants;         
+                for(var everyResData in resDataRaw){
+                    var tempObj = {}
+                    tempObj["id"] = resDataRaw[everyResData].restaurant.R.res_id
+                    tempObj["name"] = resDataRaw[everyResData].restaurant.name
+                    tempObj["rating"] = resDataRaw[everyResData].restaurant.user_rating.aggregate_rating
+                    tempObj["subLocation"] = resDataRaw[everyResData].restaurant.location.locality_verbose
+                    tempObj["cuisines"] = (resDataRaw[everyResData].restaurant.cuisines).split(", ")
+                    tempObj["thumb"] = resDataRaw[everyResData].restaurant.thumb
+                    resData.push(tempObj)
+                 }
                 this.setState({
                     resData: resData
                 })
-            } else {
-                console.log(err);
+            },
+            (error) => {
+                console.log(error);
             }
-        }.bind(this));
+        )
+    }
+
+    componentDidMount(){
+        this.dataFetcher(0);
+        this.dataFetcher(20);
+        this.dataFetcher(40);   
     }
 
     changeRes(key){
@@ -48,22 +66,26 @@ class Foodie extends React.Component {
     render() {
         return (
             <div>
-                <Grid container>
-                    <Grid item xs={4}>
-                        <div style={{height:"100%"}}>
+                <Grid container spacing={20}>
+                    
                         {
                             this.state.resData.map((value, key) => (
-                                <Card key={"res"+key} onClick={() => this.changeRes(key)} style={{display: 'flex', height: 151, marginBottom: 5}}>
+                                <Grid item xs={4}>
+                        
+                                <Card key={"res"+key}>
                                     <Grid container spacing={0}>
                                         <Grid item xs={8}>
                                             <div style={{display: 'flex', flexDirection: 'column'}}>
                                                 <CardContent>
-                                                    <Typography variant="subheading">{value.restaurant.name} #{value.restaurant.user_rating.aggregate_rating}</Typography>
+                                                    <Typography variant="subheading">{value.name} </Typography>
                                                     <Typography variant="body2" color="textSecondary">
-                                                        {value.restaurant.location.locality_verbose}
+                                                        {value.subLocation}
                                                     </Typography>
                                                     <Typography variant="caption" color="textSecondary">
-                                                        {value.restaurant.cuisines}
+                                                        {value.cuisines.join(", ")}
+                                                    </Typography>
+                                                    <Typography variant="h3">
+                                                        #{value.rating}
                                                     </Typography>
                                                 </CardContent>
                                             </div>
@@ -71,16 +93,15 @@ class Foodie extends React.Component {
                                         <Grid item xs={4}>
                                             <CardMedia
                                                 style={{width: 151, height: 151}}
-                                                image={value.restaurant.thumb}
+                                                image={value.thumb}
                                                 title="Live from space album cover"
                                             />
                                         </Grid>
                                     </Grid>
                                 </Card>
+                                </Grid>
                             ))
                         }
-                        </div>
-                    </Grid>
                 </Grid>
             </div>
         );
